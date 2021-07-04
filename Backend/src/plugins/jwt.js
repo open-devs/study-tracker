b92ef538768606f603cd78705cbe53621f7ab9ff
Mihir Kumar <mihirpandey.13@gmail.com>
2021-07-04 11:28:58 +0530

    feat: 🎸 JWT expiration everyday logic added

--- Backend/src/plugins/jwt.js
+++ Backend/src/plugins/jwt.js
@@ -5,7 +5,15 @@ const fp = require("fastify-plugin")
 // This plugins adds jwt functionality to fastify
 
 module.exports = fp(async function (fastify, opts) {
+  const d = new Date()
+  const h = d.getHours()
+  const m = d.getMinutes()
+  const s = d.getSeconds()
+  const secondsUntilEndOfDate = 24 * 60 * 60 - h * 60 * 60 - m * 60 - s
   fastify.register(require("fastify-jwt"), {
     secret: process.env.JWT_SECRET,
+    sign: {
+      expiresIn: secondsUntilEndOfDate,
+    },
   })
 })
