93308d39e03a40295f06a7fa8054df9838303673
Alok Raj <alokr417@gmail.com>
2021-07-14 14:18:06 +0530

    feat: 🎸 Subject Table Component Dashboard

--- frontend/src/components/SubjectTable.vue
+++ frontend/src/components/SubjectTable.vue
@@ -1,11 +1,78 @@
 <template>
   <div class="col-md-6 mx-auto">
-    <p>Table here</p>
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Subject</th>
+          <th scope="col">Planned Hour</th>
+          <th scope="col">Elapsed Hour</th>
+          <th scope="col">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in selectedSubject" :key="index">
+          <th scope="row">{{ index + 1 }}</th>
+          <td>{{ item.subject }}</td>
+          <td>{{ item.plannedHour }}</td>
+          <td>{{ item.elapsed }}</td>
+          <td>
+            <button
+              @click="startTimer(item)"
+              type="button"
+              class="btn btn-success btn-sm"
+            >
+              Start
+            </button>
+            |
+            <button
+              @click="stopTimer(item)"
+              type="button"
+              class="btn btn-danger btn-sm"
+            >
+              Stop
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
   name: "SubjectTable",
+  setup() {
+    // Load subjects on onMount
+    const selectedSubject = ref([
+      {
+        subject: "JavaScript",
+        plannedHour: "2 Hrs",
+        elapsed: "1 Hr",
+      },
+      {
+        subject: "DSA",
+        plannedHour: "1 Hrs",
+        elapsed: "0 Hr",
+      },
+      {
+        subject: "ReactJS",
+        plannedHour: "2 Hrs",
+        elapsed: "0 Hr",
+      },
+    ]);
+
+    const startTimer = (item) => {
+      // logic to call API call to startTimer
+      console.log("startTimer", item);
+    };
+    const stopTimer = (item) => {
+      // logic to call API call to stopTimer
+      console.log("stopTimer", item);
+    };
+    return { selectedSubject, startTimer, stopTimer };
+  },
 };
 </script>
